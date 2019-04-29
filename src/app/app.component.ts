2a2cb9ce26aa852112ad28d3116639c6c3db20d7
Noël Iyakaremye <noel.iyakaremye@lire-et-ecrire.be>
2019-04-30 01:17:00 +0200

    Exercice: une application de type blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'exercice-angular';
+
+  constructor() {
+  }
+
+  title = 'My Blog';
+  posts = [
+  {
+    title: 'Test1',
+    content: 'Mon premier post',
+    loveIts: 1,
+    created_at: new Date()
+  },
+    {
+      title: 'Test2',
+      content: 'Mon deuxième post',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Test3',
+      content: 'Mon troisième post',
+      loveIts: 0,
+      created_at: new Date()
+    }];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponentComponent } from './post-list-component/post-list-component.component';
+import {FormsModule} from '@angular/forms';
+import { PostListItemComponentComponent } from './post-list-item-component/post-list-item-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponentComponent,
+    PostListItemComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
